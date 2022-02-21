3f33afdc486ecda074567e3fb972d4e82100316b
stephen talley <stvlley.amzn@gmail.com>
2022-02-21 10:13:45 -0600

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerdePark />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
